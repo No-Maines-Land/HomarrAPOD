0b7e35658ad404ca26a31edcc81f9a6ccef710ee
No-Maines-Land <liam.roach@gmail.com>
2023-11-09 02:49:17 -0500

    Update APOD.sh

--- apod/APOD.sh
+++ apod/APOD.sh
@@ -6,7 +6,8 @@ cd /PATHTOASTROPIX
 #make image subfolder, can be removed after initial run
 mkdir astropix
 #retrieve image from url and filter out junk
-wget -nd -p --accept jpg --reject html,txt -e robots=off https://apod.nasa.gov/apod/astropix.html
+wget -nd -p --accept jpg,png --reject html,txt -e robots=off https://apod.nasa.gov/apod/astropix.html
 #change name of file for consistency
 mv *.jpg astropix/astropix.jpg
+mv *.png astropix/astropix.jpg
 #use crontab -e command and type "0 5 * * * /PATH/file" to automate at 5am every morning
